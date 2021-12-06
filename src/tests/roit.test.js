519fb10d73d4de91cad2337121b17ddff25fb928
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-12-06 17:56:46 -0300

    Adiciona testes endpoint get users

--- src/tests/roit.test.js
+++ src/tests/roit.test.js
@@ -22,6 +22,7 @@ describe('1 - Testa endpoint post "/login"', () => {
   });
 
   afterAll(async () => {
+    await db.collection('users').deleteMany({});
     await connection.close();
   });
 
@@ -57,6 +58,7 @@ describe('2 - Testa endpoint post "/user"', () => {
   });
 
   afterAll(async () => {
+    await db.collection('users').deleteMany({});
     await connection.close();
   });
 
@@ -81,7 +83,7 @@ describe('2 - Testa endpoint post "/user"', () => {
       });
   });
 
-  it('Será validado que não é possível fazer post de usuário se o token não for válido', async () => {
+  it('Será validado que não é possível fazer post de usuário se token for inválido', async () => {
     await frisby
       .setup({
         request: {
@@ -132,6 +134,116 @@ describe('2 - Testa endpoint post "/user"', () => {
             const { json } = responseLogin;
             expect(json.message).toBe('User created successfully');
           });
+      });
+  })
+});
+
+describe('4 - Testa endpoint get "/users', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(mongoDbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = connection.db('riot');
+  });
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany({});
+  });
+
+  afterAll(async () => {
+    await db.collection('users').deleteMany({});
+    await connection.close();
+  });
+
+  it('Será validado que não é possível visualizar usuários se token não for informado', async () => {
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .get(`${url}/users`)
+      .expect('status', 401)
+      .then((response) => {
+        const { body } = response;
+        result = JSON.parse(body);
+        expect(result.message).toBe('missing auth token');
+      });
+  });
+
+  it('Será validado que não é possível visualizar usuários se token for inválido', async () => {
+    await frisby
+    .setup({
+      request: {
+        headers: {
+          Authorization: '123456',
+          'Content-Type': 'application/json',
+        },
+      },
+    })
+    .get(`${url}/users`)
+    .expect('status', 401)
+    .then((response) => {
+      const { body } = response;
+      result = JSON.parse(body);
+      expect(result.message).toBe('jwt malformed');
+    });
+  });
+
+  it('Será válidado que é possível visualizar todos os usuários cadastrados', async () => {  
+    let result;
+    
+    await frisby
+      .post(`${url}/login/`, {
+        email: 'vitor@email.com',
+        password: 'admin',
+      })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        result = JSON.parse(body);
+        return frisby
+          .setup({
+            request: {
+              headers: {
+                Authorization: result.token,
+                'Content-Type': 'application/json',
+              },
+            },
+          })
+          .post(`${url}/user`, {
+            user: {
+              name: "Vitor Hugo"
+            }
+          })
+          .expect('status', 201)
+          .then((responseLogin) => {
+            const { json } = responseLogin;
+            expect(json.message).toBe('User created successfully');
+          });
+      });
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: result.token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .get(`${url}/users`)
+      .expect('status', 201)
+      .then((response) => {
+        const { body } = response;
+        result = JSON.parse(body);
+        expect(result.users).not.toBeNull();
       });
   })
 });
@@ -191,7 +303,7 @@ describe('2 - Testa endpoint post "/user"', () => {
 //           });
 //       });
 
-//     // Observação: A partir ocorre um erro que não consegui resolver
+//     // Observação: A partir daqui ocorre um erro que não consegui resolver
 //     await frisby
 //       .setup({
 //         request: {
